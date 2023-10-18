96744855743e9e57058c48a5bb1ea09874a2dced
kirillefremov <efremovk7569@gmail.com>
2023-10-18 16:38:41 +0200

    Add routes

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, url_for, render_template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/today-tasks/")
+def get_today_tasks():
+    return render_template("today_tasks.html")
+
+
+@app.route("/future-tasks/")
+def get_future_tasks():
+    return render_template("future_tasks.html")
+
+
+@app.route("/projects/")
+def get_projects():
+    return render_template("projects.html")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
