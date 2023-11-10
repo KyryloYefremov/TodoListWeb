3cecf314c8b9a93f3b87c21d91bc73d619c93f88
kirillefremov <efremovk7569@gmail.com>
2023-11-10 13:43:15 +0100

    Add creating new table in db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 db = SQLAlchemy()
@@ -11,34 +11,59 @@ db.init_app(app)
 
 
 class Task(db.Model):
+    __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     value = db.Column(db.Integer, nullable=False, default=0)
     date = db.Column(db.String)
+    # create FK
+    project_id = db.Column(db.Integer, db.ForeignKey("projects.id"))
+    # create reference btw tables
+    project = relationship(argument="Project", back_populates="tasks")
+
+
+class Project(db.Model):
+    __tablename__ = "projects"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    tasks = relationship(argument="Task", back_populates="project")
 
 
 # with app.app_context():
 #     db.create_all()
 
-# Add new tasks
+# Add new tasks and projects
+p1 = Project(
+    name="Start project"
+)
+
+p2 = Project(
+    name="School project"
+)
+
 t1 = Task(
     name="Go to supermarket",
-    date="07/11/2023"
+    date="07/11/2023",
+    project=p1
 )
 t2 = Task(
     name="Clean room",
-    date="08/11/2023"
+    date="08/11/2023",
+    project=p1
 )
 t3 = Task(
     name="Finish the project",
-    date="10/11/2023"
+    date="10/11/2023",
+    project=p2
 )
 
 # with app.app_context():
+#     db.session.add(p1)
+#     db.session.add(p2)
 #     db.session.add(t1)
-    # db.session.add(t2)
-    # db.session.add(t3)
-    # db.session.commit()
+#     db.session.add(t2)
+#     db.session.add(t3)
+#     db.session.commit()
 
 
 @app.route("/")
