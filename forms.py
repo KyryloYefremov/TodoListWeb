5bad7ade4721a1d774fbc0041785eebf13320e00
kirillefremov <efremovk7569@gmail.com>
2023-11-21 13:52:24 +0100

    Add filed for choosing project

--- forms.py
+++ forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms import StringField, SubmitField, DateField, SelectField
+from wtforms.validators import DataRequired
 
 
 class TaskForm(FlaskForm):
     name = StringField("Task Name", validators=[DataRequired()])
     date = DateField("Date", validators=[DataRequired()])
+    project = SelectField("Project",  validators=[DataRequired()])
     submit = SubmitField("Submit Task")
 
+    def set_projects(self, existing_projects: list[str]):
+        self.project.choices = existing_projects
 
