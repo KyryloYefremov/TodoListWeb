8046e43e74d4fb4b80672034c89a60095cd21f05
kirillefremov <efremovk7569@gmail.com>
2024-01-11 15:39:08 +0100

    Replace db file name with variable

--- init_program.py
+++ init_program.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 db = SQLAlchemy()
 bootstrap = Bootstrap5(app)
 # Create database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task-manager.db"
+db_file_name = "task-manager.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_file_name}"
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 # Initialise the app with the extension
 db.init_app(app)
